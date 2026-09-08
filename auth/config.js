53e8528fbfcf2bf84d144b141540136abfb5e4f7
bobruso <51798343+bobruso@users.noreply.github.com>
2026-09-08 22:51:50 +0200

    Add auth path to shared config

--- auth/config.js
+++ auth/config.js
@@ -1,4 +1,5 @@
 window.PATX_AUTH_CONFIG = {
   supabaseUrl: 'https://cnnhstlguewrxjihhlqc.supabase.co',
-  supabasePublishableKey: 'sb_publishable_uWEwYEMkAe3YkeAzX7ACAg_0aEYHmM6'
+  supabasePublishableKey: 'sb_publishable_uWEwYEMkAe3YkeAzX7ACAg_0aEYHmM6',
+  authPath: '/auth/'
 };
